6243381717afd5ce4153a099b8ac1c2f38fb2779
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-11-22 16:14:47 +0000

    Added about us page

--- survey/urls.py
+++ survey/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.SurveyView.as_view(), name='survey'),
     path('/results', views.SurveyResultsView.as_view(), name='results'),
-
+    path('/about-us', views.AboutUsView.as_view(), name='about_us'),
 ]
