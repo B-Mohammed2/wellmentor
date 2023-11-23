a4810a92adb8df08f00de796b33ecd0c70def2dc
felipesandoli <cfelipesandoli@gmail.com>
2023-11-23 18:18:53 +0000

    Create form for updating profile scores

---
+++ profiles/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfile(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = [
+            'physical_health',
+            'depression_level',
+            'relationship_health',
+            'professional_health',
+            'mental_health_awareness',
+            'anxiety_level'
+        ]
